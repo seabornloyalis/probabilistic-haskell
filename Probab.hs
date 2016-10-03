a1c414ad44014d84a4ad0bbc616687851d5cbd16
Chad Myles <chad.robert.m@gmail.com>
2016-10-03 02:24:51 -0400

    Abstracted all but deriveDists

--- Probab.hs
+++ Probab.hs
@@ -4,29 +4,83 @@
 
 import Data.List
 
+--uncomment the below line for quicker, floating point arithmetic
+--data Dist a = Dist [(a, Float)]
+
+--leave the below line uncommented for exact floating point arithmetic
+--(at some performance cost)
 data Dist a = Dist [(a,Rational)]
     deriving( Show)
 
 instance Functor Dist where
-    --fmap :: (a->b) -> Dist a -> Dist b
     fmap f (Dist xs) = Dist (map (\(x,p) -> (f x, p)) xs)
 
---dist :: [(a, Rational)] -> Dist a
---dist xs = xs
 --TODO: make sure sum of Rationals = 1 and none are negative
 
+--
+-- Public Interface
+--
+
 unit :: a -> Dist a
-unit x = Dist [(x,1.0)]
 
 uniformDist :: [a] -> Dist a
+
+weightedDist :: [(a, Rational)] -> Dist a
+
+--deDist :: Dist a -> [(a, Float)]
+deDist :: Dist a -> [(a, Rational)]
+
+mergeEqual :: Eq a => Dist a -> Dist a
+
+possibilities :: Eq a => Dist a -> [a]
+
+--probabilityOf :: Eq a => Dist a -> a -> Float
+probabilityOf :: Eq a => Dist a -> a -> Rational
+
+adjoin :: (a -> Dist b) -> Dist a -> Dist (a,b)
+
+distFil :: (a -> Bool) -> Dist a -> Dist a
+
+transform :: (a -> Dist b) -> Dist a-> Dist b
+
+combine :: Dist a -> Dist b -> Dist (a, b)
+--
+-- Implementation of Public Interface
+--
+
+--unit :: a -> Dist a
+unit x = Dist [(x,1.0)]
+
+--uniformDist :: [a] -> Dist a
 uniformDist xs = Dist (map (\ x -> (x, (1.0 / len))) xs)
     where len = fromIntegral (length xs)
 
-{-uniformDist :: [a] -> [(a, Rational)]
-uniformDist xs = map (\ x -> (x, (1.0 / len))) xs
-    where len = fromIntegral (length xs)-}
+--weightedDist :: [(a, Rational)] -> Dist a
+weightedDist xs = Dist xs
 
-adjoin :: (a -> Dist b) -> Dist a -> Dist (a,b)
+--deDist :: Dist a -> [(a, Rational)]
+deDist (Dist valList ) = valList
+
+--mergeEqual :: Eq a => [(a, Rational)] -> [(a, Rational)]
+mergeEqual xs =
+    let distinct = possibilities xs
+    in weightedDist (map (\y -> (y, (foldl (\acc (b,c) ->if (b == y)
+                                                        then (acc + c)
+                                                        else acc) 0.0 (deDist xs)))) distinct)
+
+--possibilites :: Eq a => Dist a -> [a]
+possibilities xs =
+    let firsts = map (\(a,b) -> a) (deDist xs)
+    in nub firsts
+
+--probabilityOf :: Eq a => [(a, Rational)] -> a -> Rational
+probabilityOf xs x = 
+    let mergedDist = mergeEqual xs
+    in if (x `elem` (possibilities mergedDist))
+        then snd (head (filter (\(f,s) -> f == x) (deDist mergedDist)))
+        else 0.0  
+
+--adjoin :: (a -> Dist b) -> Dist a -> Dist (a,b)
 adjoin f (Dist xs) =
             let wrapped = map (\(k, p) ->
                             let (Dist res) = f k
@@ -35,15 +89,25 @@ adjoin f (Dist xs) =
                                     xs
             in Dist (concat wrapped)
 
-{-combine :: [(a, Rational)] -> [(b, Rational)] -> [((a, b), Rational)]
-combine xs ys = concat (map (\ x -> map (\ y -> ((fst x, fst y), snd x * snd y)) ys) xs)-}
+--distFil :: (a -> Bool) -> Dist a -> Dist a
+distFil f xs =
+    let intermed = filter (\ x -> f (fst x)) (deDist xs)
+    in weightedDist (map (\ (x,y) -> (x, y * (1.0 / totalProb(intermed)))) intermed)
 
-{-duplicateDist :: Integral a => a -> Dist b -> Dist [b]
-duplicateDist num xs
-    | num <= 0      =error "Can only call duplicateDist with positive integers"
-    | num == 1      = map (\a -> [a]) xs
-    | otherwise     = adjoin (\a' -> xs) (duplicateDist (num-1) xs)
--}
+--transform :: (a -> Dist b) -> Dist a-> Dist b
+transform f xs = weightedDist (concat (map (\([(x, y)], z) -> [(x, y*z)]) (map (\ (a,b) -> (deDist (f a), b)) (deDist xs))))
+
+--combine :: Dist a -> Dist b -> Dist (a, b)
+combine xs ys = weightedDist (concat (map (\ x -> map (\ y -> ((fst x, fst y), snd x * snd y)) (deDist ys)) (deDist xs)))
+
+--deriveDists :: Integral a => Dist a -> Dist b -> Dist [b]
+--deriveDists nums xs = concat (map (\([(x,y)],z) -> [(x,y*z)]) (map (\(a, b) -> ((duplicateDist a xs), b)) nums))
+--
+-- Private functions
+--
+totalProb :: [(a,Rational)] -> Rational
+totalProb xs = foldl (\acc (a,b) -> acc + b) 0.0 xs
+
 {-duplicateDist :: Integral a => a -> [(b, Rational)] -> [([b], Rational)]
 duplicateDist num xs = duplicateDistHelper num xs
 
@@ -51,49 +115,9 @@ duplicateDistHelper :: Integral a => a -> [(b, Rational)] -> [([b], Rational)]
 duplicateDistHelper num xs = if (num == 1)
                             then map (\(a,b) -> ([a], b)) xs
                             else map (\((a,b),c) -> (a:b,c)) (combine xs (duplicateDistHelper (num-1) xs))
--}
-{-mapDist :: (a -> b) -> Dist a -> Dist b
-mapDist f xs = map (\ (x,y) -> (f x, y)) xs
-
-transform :: (a -> [(b, Rational)]) -> [(a, Rational)] -> [(b, Rational)]
-transform f xs = concat (map (\([(x, y)], z) -> [(x, y*z)]) (map (\ (a,b) -> (f a, b)) xs))
-
-distFil :: (a -> Bool) -> [(a, Rational)] -> [(a, Rational)]
-distFil f xs =
-    let intermed = filter (\ x -> f (fst x)) xs
-    in map (\ (x,y) -> (x, y * (1.0 / totalProb(intermed)))) intermed
-
-
-mergeEqual :: Eq a => [(a, Rational)] -> [(a, Rational)]
-mergeEqual xs =
-    let distinct = possibilites xs
-    in map (\d -> (d, foldl (\acc (x,y) -> if (x == d)
-                                           then (acc + y)
-                                           else (acc))
-                            0.0 xs)) distinct
-
-
-deDist :: [(a, Rational)] -> [a]
-deDist xs = map (\(x, y) -> x) xs
-
-possibilites :: Eq a => [(a, Rational)] -> [a]
-possibilites xs = nub (deDist xs)
-
-probabilityOf :: Eq a => [(a, Rational)] -> a -> Rational
-probabilityOf xs x = 
-    let mergedDist = mergeEqual xs
-    in if (x `elem` (deDist mergedDist))
-        then snd (head (filter (\(f,s) -> f == x) (mergedDist)))
-        else 0.0  
 
 deriveDists :: Integral a => [(a, Rational)] -> [(b, Rational)] -> [([b], Rational)]
 deriveDists nums xs = concat (map (\([(x,y)],z) -> [(x,y*z)]) (map (\(a, b) -> ((duplicateDist a xs), b)) nums))
-
-totalProb :: [(a,Rational)] -> Rational
-totalProb = sanityCheck
-
-sanityCheck :: [(a,Rational)] -> Rational
-sanityCheck xs = foldl (\acc (a,b) -> acc + b) 0.0 xs
 -}
 --A
 {-solutionAd6 :: [(Int, Rational)]
