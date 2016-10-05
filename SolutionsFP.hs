6064b8d5ab2a15885eae20c76ef29c38728904ec
Chad Myles <chad.robert.m@gmail.com>
2016-10-05 00:09:19 -0400

    Fixed showsolution / runsolution bug

--- SolutionsFP.hs
+++ SolutionsFP.hs
@@ -15,24 +15,24 @@ showSolution "A" =
     let a = "Solution A part 1: " ++ (show solutionAd6) ++ "\n"
         b = "Solution A part 2: " ++ (show solutionAd12) ++ "\n"
     in a ++ b
-runSolution "B" = "Solution B: " ++ (show solutionB) ++ "\n"
-runSolution "C" = 
+showSolution "B" = "Solution B: " ++ (show solutionB) ++ "\n"
+showSolution "C" = 
     let params = uniformDist [6,8,10,12,20]
         c1 = "Solution C part 1: " ++ (show (solutionC1 params)) ++ "\n"
         c2 = "Solution C part 2: " ++ (show (solutionC2 params)) ++ "\n"
     in "For uniform distribution of d6, d8, d10, d12, d20: \n" ++ c1 ++ c2
-runSolution "D" =
+showSolution "D" =
     let d = "Solution D: " ++ (show (solutionD (uniformDist [6,8,10,12,20]))) ++ "\n"
     in "For uniform distribution of d6, d8, d10, d12, d20: \n" ++ d
-runSolution "F" = "Solution F: " ++ (show solutionF) ++ "\n"
-runSolution "G" = "Solution G: " ++ (show solutionG) ++ "\n"
-runSolution "I" = "Solution I: " ++ (show solutionI) ++ "\n"
-runSolution "J" = "Solution J: " ++ (show solutionJ) ++ "\n"
-runSolution "K" = "Solution K: " ++ (show solutionK) ++ "\n"
-runSolution "N" =
+showSolution "F" = "Solution F: " ++ (show solutionF) ++ "\n"
+showSolution "G" = "Solution G: " ++ (show solutionG) ++ "\n"
+showSolution "I" = "Solution I: " ++ (show solutionI) ++ "\n"
+showSolution "J" = "Solution J: " ++ (show solutionJ) ++ "\n"
+showSolution "K" = "Solution K: " ++ (show solutionK) ++ "\n"
+showSolution "N" =
     let n = "Solution N: " ++ (show (solutionN (uniformDist [6,8,10,12,20]))) ++ "\n"
     in "For uniform distribution of d6, d8, d10, d12, d20: \n" ++ n
-runSolution x = "Solution not implemented or argument invalid"
+showSolution x = "Solution not implemented or argument invalid"
 
 --A
 solutionAd6 :: Dist Int
